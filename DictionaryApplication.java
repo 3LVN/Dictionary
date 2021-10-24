d39f28f5b0dec9de82a06aca938908b707fc4b19
3LVN <59151135+3LVN@users.noreply.github.com>
2021-10-25 04:56:20 +0700

    Add files via upload

--- DictionaryApplication.java
+++ DictionaryApplication.java
@@ -1,5 +1,37 @@
-public class DictionaryApplication {
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+public class DictionaryApplication{
+    JTextField search;
+    DictionaryCommandline com = new DictionaryCommandline();
     public void runApplication() {
-        
+        DefaultListModel<String> target = new DefaultListModel<>();
+        com.man.insertFromFile();
+        for(int  i = 0; i < com.man.dict.Word.size(); i++){
+            Word list = com.man.dict.Word.get(i);
+            String s = list.getTarget();
+            s= s.split("\\s")[0];
+            target.addElement(s);
+        }
+        JList<String> list = new JList<>(target);
+        final JFrame f = new JFrame();
+        f.setSize(800, 600);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        f.getContentPane().setLayout(new FlowLayout());
+        list.setSize(200, 500);
+        list.setLocation(0,100);
+        JScrollPane scr = new JScrollPane(list);
+
+        scr.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        f.getContentPane().add(scr);
+
     }
+    public static void main(String[] args) {
+        DictionaryApplication app = new DictionaryApplication();
+        app.runApplication();
+    }
+
 }
